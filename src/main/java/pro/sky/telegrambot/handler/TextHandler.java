29ecfb17f117bff8b7ab1949b6d5b2d375466f55
mcharliev <mcharliev@gmail.com>
2023-04-10 21:18:30 +0300

    3 ЭТАП

--- src/main/java/pro/sky/telegrambot/handler/TextHandler.java
+++ src/main/java/pro/sky/telegrambot/handler/TextHandler.java
@@ -1,6 +1,11 @@
 package pro.sky.telegrambot.handler;
 
+import pro.sky.telegrambot.entity.ContactDetails;
 import pro.sky.telegrambot.entity.Owner;
+import pro.sky.telegrambot.enums.PetType;
+import pro.sky.telegrambot.enums.ProbationaryStatus;
+import pro.sky.telegrambot.menu.InlineKeyboard;
+import pro.sky.telegrambot.service.ContactDetailsService;
 import pro.sky.telegrambot.service.OwnerService;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.model.Message;
@@ -10,6 +15,8 @@ import com.pengrad.telegrambot.request.SendMessage;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * "Перехватчик" updates типа Text
@@ -19,42 +26,71 @@ public class TextHandler implements Handler {
     private final TelegramBot telegramBot;
     private final OwnerService ownerService;
 
-    public TextHandler(TelegramBot telegramBot, OwnerService ownerService) {
+    private final ContactDetailsService contactDetailsService;
+
+    private final Pattern pattern = Pattern.compile("\\d{11} [А-я]+");
+
+    public TextHandler(TelegramBot telegramBot,
+                       ContactDetailsService contactDetailsService,
+                       OwnerService ownerService) {
         this.telegramBot = telegramBot;
+        this.contactDetailsService = contactDetailsService;
         this.ownerService = ownerService;
     }
 
     @Override
-    public void handle(Update update) throws IOException {
+    public void handle(Update update)  {
         Message message = update.message();
         Long chatId = message.chat().id();
         String text = message.text();
-        Owner owner = ownerService.findOwnerByChatId(chatId);
-        LocalDateTime date = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
-        Long VOLUNTEER_CHAT_ID = 5102380657L;
+        String name = message.chat().firstName();
+        Matcher matcher = pattern.matcher(text);
+        LocalDateTime dateOfStartProbation = LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES);
+        LocalDateTime dateOfEndProbation = dateOfStartProbation.plusDays(30);
+        InlineKeyboard inlineKeyboard = new InlineKeyboard(telegramBot);
 
-        if (owner != null && text.length() > 100) {
-            owner.setStringReport(text);
-            owner.setDateOfLastReport(date);
-            ownerService.addOwner(owner);
-            if (owner.getPhotoReport() == null) {
-                sendMessage(chatId, "Пожалуйста, добавьте фото к вашему отчету");
-            }
-            if (text.length() < 100) {
-                sendMessage(chatId, "Пожалуйста предоставьте более подробный отчет");
+        if ("/start".equals(text)) {
+            inlineKeyboard.showStartMenu(chatId);
+        } else if ("/saveDogOwner".equals(text)) {
+            PetType petType = PetType.DOG;
+            ownerService.saveNewDogOwner(chatId,
+                    name,
+                    petType,
+                    dateOfStartProbation,
+                    dateOfEndProbation,
+                    dateOfStartProbation,
+                    ProbationaryStatus.ACTIVE
+            );
+            sendMessage(chatId, "Вы успешно зарегестрировались, ваши данные" +
+                    "\nваше имя: " + name + " \nтип животного: " + petType);
+        } else if (matcher.find()) {
+            String result = matcher.group(0);
+            findMatchesAndSaveInBd(result, chatId);
+        } else {
+            Owner owner = ownerService.findOwnerByChatId(chatId);
+            if (owner != null && text.length() > 30) {
+                owner.setStringReport(text);
+                owner.setDateOfLastReport(dateOfStartProbation);
+                ownerService.saveOwner(owner);
+                sendMessage(chatId,"Вы успешно загрузили тесктовый отчет");
+                if (owner.getPhotoReport() == null) {
+                    sendMessage(chatId, "Пожалуйста не забудьте предоставить фото отчет");
+                }
+            } else {
+                sendMessage(chatId, "Отчет недостаточно подробный, пожалуйста заполните отчет подробнее");
             }
         }
-
-        if(owner.getDateOfLastReport().plusDays(2).isBefore(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES))){
-            telegramBot.execute(
-                    new SendMessage(owner.getChatId(), "Дорогой усыновитель, мы заметили, что вы заполняете отчет не так подробно, " +
-                            "как необходимо. Пожалуйста, подойди ответственнее к этому занятию.  " +
-                            "В противном случае волонтеры приюта будут обязаны самолично " +
-                            "проверять условия  содержания собаки"));
-
-            sendMessage(VOLUNTEER_CHAT_ID, "Усыновитель id: " + owner.getChatId() +
-                    " не присылает отчеты в течение двух дней, пожалуйста свяжитесь с ним");
-        }
+    }
+    //Когда паттер сработал, метод обрезает вытаскивает из стринга имя и телефон и записывает результат в БД
+    private void findMatchesAndSaveInBd(String foundString, Long chatId) {
+        foundString = foundString.replaceAll(" ", "");
+        String phoneNumber = foundString.substring(0, 10);
+        String name = foundString.substring(11);
+        ContactDetails contactDetails = new ContactDetails();
+        contactDetails.setChatId(chatId);
+        contactDetails.setPhoneNumber(phoneNumber);
+        contactDetails.setName(name);
+        contactDetailsService.save(contactDetails);
     }
 
     private void sendMessage(Long chatId, String message) {
